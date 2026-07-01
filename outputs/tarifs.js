2467a6e8fd02c69c95e7e1e819f834bba31091a7
FloFish44 <flogilet44@gmail.com>
2026-07-01 10:48:54 +0200

    Ajoute le tarif de base (#2)
    Co-authored-by: FloFish44 <215822370+FloFish44@users.noreply.github.com>

--- outputs/tarifs.js
+++ outputs/tarifs.js
@@ -1,7 +1,17 @@
 window.TARIF_CONFIG = {
   senderName: "Schuller – Ne pas répondre",
   senderEmail: "schullerrosheim@gmail.com",
-  tariffName: "Tarif 50 + 10",
-  driveFileId: "1TKbzWxi1Oc1dVO2kbUWl8YWgcuKFIKwG",
   endpoint: "https://script.google.com/macros/s/AKfycbwoAYkVxWm6o_lBn1XSHz1MTIHG_m_cQALlSRsEiAfqTl_AMVpoSXxPja7DZKOgSPinVg/exec",
+  tariffs: [
+    {
+      id: "tarif-50-plus-10",
+      name: "Tarif 50 + 10",
+      driveFileId: "1TKbzWxi1Oc1dVO2kbUWl8YWgcuKFIKwG",
+    },
+    {
+      id: "tarif-de-base",
+      name: "Tarif de base",
+      driveFileId: "1wnUuhEyPYRQ733jqwLpu6nsq6TnppM_5",
+    },
+  ],
 };
